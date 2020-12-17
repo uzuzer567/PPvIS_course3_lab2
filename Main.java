3c3b3a3e58ccf9d0aa7577a7c34a5d46a54d3de5
uzuzer567 <49733302+uzuzer567@users.noreply.github.com>
2020-12-17 13:01:44 +0300

    add main file

---
+++ Main.java
@@ -0,0 +1,13 @@
+import Model.Manager;
+
+public class Main {
+    public void start()
+    {
+        new Manager();
+    }
+
+    public static void main(String[] args) {
+        Main main = new Main();
+        main.start();
+    }
+}
